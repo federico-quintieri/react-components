99d3ecae97fb63f66fd7d4d2bc9a54b5fef7894d
federico-quintieri <99342330+fede-quinty@users.noreply.github.com>
2024-12-12 14:38:46 +0100

    feat:almost done

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Header from "./components/bodyHeader";
+import Card from "./components/bodyCard";
+import Footer from "./components/bodyfooter";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-
-  </StrictMode>,
-)
+    <Header />
+    <Card />
+    <Footer />
+  </StrictMode>
+);
